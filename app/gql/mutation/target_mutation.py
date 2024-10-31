4f3edf4ecc10ddb0348acd82959db014c8c5ba09
reuven <r0583247229@gmail.com>
2024-10-31 15:00:08 +0200

    add graphql func with mutations

--- app/gql/mutation/target_mutation.py
+++ app/gql/mutation/target_mutation.py
@@ -1,12 +1,11 @@
 from graphene import  Int, String, Field, Mutation
 
 from app.gql.type.target_type import TargetType
-from app.repository.mission_repository import add_target
+from app.repository.target_repository import add_target
 
 
 class AddTarget(Mutation):
     class Arguments:
-        target_id = Int()
         target_industry = String()
         city_id = Int()
         target_type_id = Int()
@@ -16,5 +15,5 @@ class AddTarget(Mutation):
 
     @staticmethod
     def mutate(root, info, **kwargs):
-        inserted_target = add_target(**kwargs)
+        inserted_target = add_target(**kwargs).value_or(None)
         return AddTarget(Target=inserted_target)
--- app/gql/schema.py
+++ app/gql/schema.py
@@ -1,5 +1,5 @@
 from graphene import ObjectType, Schema
-from app.gql.mutation.mission_mutation import AddMission
+from app.gql.mutation.mission_mutation import AddMission, UpdateMission, DeleteMission
 from app.gql.mutation.target_mutation import AddTarget
 from app.gql.query.mission_query import MissionQuery
 
@@ -7,6 +7,8 @@ from app.gql.query.mission_query import MissionQuery
 class Mutations(ObjectType):
     add_mission = AddMission.Field()
     add_target = AddTarget.Field()
+    update_mission = UpdateMission.Field()
+    delete_mission = DeleteMission.Field()
 
 class Query(MissionQuery):
     pass
